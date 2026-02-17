d0f0f9cfe479997a830304955982bb44d9dc69bd
Vipershroom <undertalefan812@gmail.com>
2026-02-17 16:25:14 -0500

    Add pokemon structs

--- src/pokemon/pokemon.rs
+++ src/pokemon/pokemon.rs
@@ -1,1 +1,59 @@
-struct Pokemon {}
+use crate::input;
+
+
+// Used for constructing the full stats of a pokemon
+pub struct PokemonBaseStats {
+    name: String,
+    types: (String, String),
+    ability: String,
+    hp: u16,
+    attack: u16,
+    defense: u16,
+    spatk: u16,
+    spdef: u16,
+    speed: u16,
+    
+    
+}
+
+// The resulting pokemon when fully IV'd, EV'd, and item'd
+pub struct Pokemon {
+    name: String,
+    types: (String, String),
+    ability: String,
+    hp: u16,
+    attack: u16,
+    defense: u16,
+    spatk: u16,
+    spdef: u16,
+    speed: u16,
+    nature: String,
+    IV: Vec<u16>,
+    EV: Vec<u16>,
+    item: String,
+}
+
+impl Pokemon {
+    pub fn new(name: String, types: (String, String), ability: String,
+    hp: u16, attack: u16, defense: u16, spatk: u16, spdef: u16, speed: u16, nature: String,
+    IV: Vec<u16>, EV: Vec<u16>, item: String) -> Pokemon 
+    {
+        Pokemon { name, types, ability, hp, attack, defense, spatk, spdef, speed, nature, IV, EV, item }
+    }
+
+    fn IV(&mut self) {
+        loop {
+            println!("Please enter your IV's in order of HP to speed seperated by comma\n IE, 31, 25, 20, 31, 25, 31");
+            let iv = input();
+            let iv_int = iv.split(",").map(|x| {
+                let m = x.parse::<u16>().unwrap_or_else(|error| {
+                    println!("Please enter numbers for your IV's, setting default to 31");
+                    return 31
+                });
+                m
+            });
+            self.IV = iv_int.collect()
+        }
+        
+    }
+}
