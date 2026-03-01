90b53fd533f44177e97e59ce79705f9436818a07
Vipershroom <undertalefan812@gmail.com>
2026-03-01 00:09:04 -0500

    I want a sandwich

--- src/pokemon/fetch.rs
+++ src/pokemon/fetch.rs
@@ -8,7 +8,6 @@ use crate::pokemon::pokemon::PokemonBaseStats;
 pub struct PokemonJson {
     abilities: String,
     moves: String,
-    species: String,
     stats: String,
     types: String,
 }
@@ -27,7 +26,22 @@ pub async fn parse_pokemon(poke_json: String) -> Result<PokemonJson, serde_json:
     let abilities = poke["abilities"].as_array().map(|arr| {
         arr.iter().filter_map(|item| item["ability"]["name"].as_str()).collect::<Vec<_>>().join(", ")
     }).unwrap_or_default();
-    println!("{}", abilities);
+
+    let moves = poke["moves"].as_array().map(|arr| {
+        arr.iter().filter_map(|mv| mv["move"]["name"].as_str()).collect::<Vec<_>>().join(", ")
+    }).unwrap_or_default();
+
+    let moves = poke["moves"].as_array().map(|arr| {
+        arr.iter().filter_map(|mv| mv["move"]["name"].as_str()).collect::<Vec<_>>().join(", ")
+    }).unwrap_or_default();
+
+    let stats = poke["stats"].as_array().map(|arr| {
+        arr.iter().filter_map(|st| Some((st["stat"]["name"].as_str(),st["base_stat"].to_string().parse::<u16>()))).collect::<Vec<_>>()
+    }).unwrap_or_default();
+    let s = stats.iter().map(|x| {
+        (x.0.unwrap().to_string(), x.1.as_ref().unwrap().to_owned())
+    }).collect::<Vec<(String,u16)>>();
+    println!("{:?}", s);
     todo!("")
 
 }
